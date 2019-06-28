ca578abf5f3ab4b7dc77919ac1cb8741b3370c64
Jury Kakov <kapkov555@gmail.com>
2019-06-28 14:13:43 +0300

    фикс стилей логин формы и инициализация пользователя в пустую бд

---
+++ src/main/resources/migration/insert_users.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (login, name, surname, date_create, mail, password, role)
+VALUES ('root', 'Jury', 'Kapkov', CURRENT_TIMESTAMP , 'kapkov555@gmail.com', '$2a$10$LijUmixpYL0i9rRvwXrnX.heUijboQzE3PsoCrxuJANIDVX28FNjS', 'ROLE_ADMIN')
