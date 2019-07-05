b8a316de09d76fdbde4c06bb1f25543a57b8fafd
Jury Kakov <kapkov555@gmail.com>
2019-07-05 18:22:10 +0300

    Сделал вывод старого текста в поле изменения названия метки

--- src/main/resources/static/js/tag.js
+++ src/main/resources/static/js/tag.js
@@ -37,3 +37,31 @@ $(document).ready(function(){
             }
         });
 });
+
+ function edit(element) {
+        var button = document.getElementById("action_button");
+        var id = element.id;
+        var data = element.dataset.text;
+        if (button.textContent == "Добавить"){
+                var temp = document.getElementById("id_for_editing");
+                temp.value = id;
+                var edit_input = document.getElementById("data_for_editing");
+                edit_input.value = data;
+
+                var edit_form = document.getElementById("tag-edit-form");
+                var add_form = document.getElementById("tag-add-form");
+                edit_form.classList.remove("display_none");
+                edit_form.classList.add("display_flex");
+
+                add_form.classList.remove("display_flex");
+                add_form.classList.add("display_none");
+
+                var button_edit = document.getElementById("action_button_edit");
+                button_edit.classList.add("add_button");
+        } else {
+            var temp = document.getElementById("id_for_editing");
+            temp.value = id;
+            var edit_input = document.getElementById("data_for_editing");
+            edit_input.value = data;
+        }
+    }
